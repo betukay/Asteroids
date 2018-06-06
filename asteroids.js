6f7bfe98aa73f06379d0ab927728ce8a9597e5b2
Betu <bkay210@gmail.com>
2018-06-05 23:36:35 -0400

    add asteroids

--- asteroids.js
+++ asteroids.js
@@ -1,69 +1,13 @@
-var ship;
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  ship = new Ship();
-}
-
-function draw() {
-  background(0);
-  ship.render();
-  ship.turn();
-  ship.update();
-}
-
-function keyReleased() {
-  ship.setRotation(0.0);
-  ship.boosting(false);
-}
-
-function keyPressed() {
-  if (keyCode == RIGHT_ARROW) {
-    ship.setRotation(0.1);
-  } else if (keyCode == LEFT_ARROW) {
-    ship.setRotation(-0.1);
-  } else if (keyCode == UP_ARROW ) {
-    ship.boosting(true);
-  }
-}
-
-function Ship () {
-  this.pos = createVector(width/2, height/2);
-  this.r = 20;
-  this.heading = 0;
-  this.rotation = 0;
-  this.vel = createVector(0, 0);
-  this.isBoosting = false;
-
-  this.boosting = function(b) {
-    this.isBoosting = b;
-  }
-
-  this.update = function() {
-    this.boost();
-    this.pos.add(this.vel);
-    this.vel.mult(0.95);
-  }
-
-  this.boost = function() {
-    var force = p5.Vector.fromAngle(this.heading);
-    this.vel.add(force);
-  }
+function Asteroid() {
+  this.pos = createVector(random(width), random(height))
+  this.r = 50;
 
   this.render = function() {
-    translate(this.pos.x, this.pos.y);
-    rotate(this.heading + PI / 2);
-    noFill();
+    push();
     stroke(255);
-    triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
-  }
-
-  this.setRotation = function(a){
-    this.rotation = a;
-  }
-
-  this.turn = function() {
-    this.heading += this.rotation;
-
+    noFill();
+    translate(this.pos.x, this.pos.y);
+    ellipse(0,0 this.r * 2);
+    pop();
   }
 }
---
+++ draw.js
@@ -0,0 +1,35 @@
+var ship;
+var asteroids = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  ship = new Ship();
+  asteroids.push(new Asteroid());
+}
+
+function draw() {
+  background(0);
+  ship.render();
+  ship.turn();
+  ship.update();
+  ship.edges();
+
+  for(var i = 0; i < asteroids.length; i++){
+    asteroids[i].render();
+  }
+}
+
+function keyReleased() {
+  ship.setRotation(0.0);
+  ship.boosting(false);
+}
+
+function keyPressed() {
+  if (keyCode == RIGHT_ARROW) {
+    ship.setRotation(0.1);
+  } else if (keyCode == LEFT_ARROW) {
+    ship.setRotation(-0.1);
+  } else if (keyCode == UP_ARROW ) {
+    ship.boosting(true);
+  }
+}
