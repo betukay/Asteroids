f3e9c7ebb18e41d404e266a53e570c19a12cde24
Betu <bkay210@gmail.com>
2018-07-07 23:26:57 -0400

    add breakup

--- asteroids.js
+++ asteroids.js
@@ -1,15 +1,20 @@
-function Asteroid(pos) {
+function Asteroid(pos, r) {
   if (pos) {
     this.pos = pos.copy();
   } else {
     this.pos = createVector(random(width), random(height));
   }
+  if (r) {
+    this.r = r * 0.5;
+  } else {
+    this.r = random(15, 50);
+  }
+
   this.vel = p5.Vector.random2D();
-  this.r = random(15, 50);
   this.total = floor(random(5, 15));
   this.offset = [];
   for (var i = 0; i < this.total; i++) {
-    this.offset[i] = random(-5, 15);
+    this.offset[i] = random(-this.r * 0.5, this.r);
   }
 
   this.update = function() {
@@ -37,8 +42,8 @@ function Asteroid(pos) {
 
   this.breakup = function() {
     var newA = [];
-    newA[0] = new Asteroid(this.pos);
-    newA[1] = new Asteroid(this.pos);
+    newA[0] = new Asteroid(this.pos, this.r);
+    newA[1] = new Asteroid(this.pos, this.r);
     return newA;
   }
 
